b9cabf7e337444fb4e8e1989893886971ea95357
Duc Trung Vu <wtrungvu@gmail.com>
2022-03-31 17:00:33 +0700

    Add SpringBootWebSocketChatApp ✨

---
+++ src/main/java/com/wtrungvu/SpringBootWebSocketChatApp/SpringBootWebSocketChatAppApplication.java
@@ -0,0 +1,13 @@
+package com.wtrungvu.SpringBootWebSocketChatApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebSocketChatAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebSocketChatAppApplication.class, args);
+	}
+
+}
